08eac5f9c1ff1cec7a128dcf2582858869f60d05
Renato <renatoresin.22@gmail>
2022-06-05 23:46:27 -0300

    first commit

---
+++ ex-arrays.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   char letra[11];
+   letra[0] = 'L';
+   letra[1] = 'I';
+   letra[2] = 'N';
+   letra[3] = 'G';
+   letra[4] = 'U';
+   letra[5] = 'A';
+   letra[6] = 'G';
+   letra[7] = 'E';
+   letra[8] = 'M';
+   letra[9] = ' ';
+   letra[10] = 'C';
+   
+   
+	 
+
+	for(int i = 0; i < 11; i++){
+			printf("%c", letra[i]);
+	}
+	
+	
+	
+	
+}
